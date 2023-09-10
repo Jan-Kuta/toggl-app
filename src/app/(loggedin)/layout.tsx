488e2f717e99601bdb05421106f990029133c5fb
Jan Kuta <jan.kuta@email.cz>
2023-09-10 15:47:05 +0200

    Updated layout

--- src/app/(loggedin)/layout.tsx
+++ src/app/(loggedin)/layout.tsx
@@ -3,6 +3,7 @@ import Link from 'next/link'
 import { useEffect } from 'react'
 import { redirect } from 'next/navigation'
 import { getUserName } from '@/helpers/getUserName'
+import { Header } from '@/components/Header'
 
 export default function LoggedinLayout({
                                      children,
@@ -18,20 +19,11 @@ export default function LoggedinLayout({
   }, [userName])
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <nav>
-        <Link href={'/projects'}>
-          <span className="btn btn-link">Projects</span>
-        </Link>
-        <Link href={'/reports'}>
-          <span className="btn btn-link">Reports</span>
-        </Link>
-        <Link href={'/time'}>
-          <span className="btn btn-link">Time</span>
-        </Link>
-      </nav>
-      <div className="divider" />
-      {children}
+    <main>
+      <Header />
+      <div className="min-h-screen p-4 max-w-screen-xl lg:mx-auto">
+        {children}
+      </div>
     </main>
   )
 }
